76cef946e143fe4ba339c700131af0e33baee292
RitikKumar202 <ritik2002july@gmail.com>
2022-12-13 01:05:10 +0530

    Creating appointment form

---
+++ src/Form.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export default function form() {
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <p><span>Book Appointment</span></p>
+        <p>Check out our availability and book the date and time that works for you</p>
+      </div>
+      <div className="details">
+        <div className="title">
+          <p>Patients Details</p>
+        </div>
+        <div className="patients-info">
+          <div className="info">
+            <label htmlFor="">First Name<span>*</span></label>
+            <input type="text" required />
+          </div>
+          <div className="info">
+            <label htmlFor="">Last Name<span>*</span></label>
+            <input type="text" required />
+          </div>
+          <div className="info">
+            <label htmlFor="">Phone Number<span>*</span></label>
+            <input type="tel" required />
+          </div>
+          <div className="info">
+            <label htmlFor="">Email<span>*</span></label>
+            <input type="email" required />
+          </div>
+          <div className="info">
+            <label htmlFor="">Gender<span>*</span></label>
+            <select name="" id="" required>
+              <option value="" selected>Choose Gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="slot">
+        <div className="title">
+          <p>Check Availability</p>
+        </div>
+      </div>
+    </div>
+  )
+}
